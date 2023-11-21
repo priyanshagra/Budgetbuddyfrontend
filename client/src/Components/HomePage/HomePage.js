d236022ac23436a33a0b76587a4709b87dda8081
Madhurgupta12 <om96002@gmail.com>
2023-11-21 14:36:16 +0530

    Changes Done by me

--- client/src/Components/HomePage/HomePage.js
+++ client/src/Components/HomePage/HomePage.js
@@ -1,14 +1,53 @@
-import React from 'react'
+// Navbar.js
+import React from 'react';
+import styled from 'styled-components';
+import ExpenseTracker from './ExpenseTracker';
+
+const NavbarContainer = styled.div`
+  background-color: #24292e;
+  color: white;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const NavLink = styled.a`
+  color: white;
+  text-decoration: none;
+  margin: 0 15px;
+  font-weight: bold;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const HomePage = () => {
   return (
-    <div className="bg-red-500 flex justify-between header" >
-        <div className=" text-3xl font-bold">Home</div>
-        <div className=" text-3xl font-bold">About</div>
-        <div className=" text-3xl font-bold">Service</div>
+    <NavbarContainer>
+      <Logo>GitHub</Logo>
+      <NavLinks>
+        <NavLink href="#">Pull requests</NavLink>
+        <NavLink href="#">Issues</NavLink>
+        <NavLink href="#">Marketplace</NavLink>
+        <NavLink href="#">Explore</NavLink>
+      </NavLinks>
+    </NavbarContainer>
+   
+  );
+};
 
-    </div>
-  )
-}
+
 
 export default HomePage
