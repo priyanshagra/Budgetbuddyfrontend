62ecb8e39834f3fb2ef8688270095fca2a62da2c
RSGPhoeniX01 <rpx03775@gmail.com>
2023-11-25 00:35:48 +0530

    added theme to otp onbding signin signup

--- client/src/Components/Onboarding.js
+++ client/src/Components/Onboarding.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from './budgetbuddysignin.jpeg'
+import logo from "./budgetbuddysignin.jpeg";
 
 import {
   Container,
@@ -13,8 +13,10 @@ import {
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import { useToast } from "@chakra-ui/react";
+import { CryptoState } from "./CryptoContext";
 
 const Onboarding = () => {
+  const { isSwitchOn, setIsSwitchOn } = CryptoState();
   const [name, setName] = useState();
   const [confirmpassword, setConfirmpassword] = useState();
   const [password, setPassword] = useState();
@@ -115,130 +117,173 @@ const Onboarding = () => {
 
   return (
     <div>
-      <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-stone-800 via-stone-400 to-neutral-700">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm bg-neutral-200 h-full m-5 p-8 shadow-lg">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Register to
-          </h2>
-          <h2 className="text-center text-4xl font-bold text-white mb-6 animate-pulse">
-        <span className="text-green-700">Budget Buddy</span>
-        </h2>
-        </div>
-
-          <div className="flex flex-row justify-center items-center">
-            <div className="rounded-lg p-2 bg-pink-100 hover:bg-black transition">
-              <img src={logo} alt="" className="h-20 w-20" />
-            </div>
+      <div
+        className={`h-screen w-screen flex ${
+          isSwitchOn
+            ? "bg-gradient-to-r from-neutral-400 via-white to-neutral-400"
+            : "bg-gradient-to-r from-gray-700 via-black to-gray-700"
+        }`}
+      >
+        <div
+          className={`rounded-full border-2 h-90 bg-back flex-1 flex-col justify-center px-6 py-12 lg:px-8 ${
+            isSwitchOn
+              ? "bg-gradient-to-r from-neutral-400 via-white to-neutral-400"
+              : "bg-gradient-to-r from-gray-700 via-black to-gray-700"
+          }`}
+        >
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            
+        <h2 className="text-center text-4xl font-bold text-white animate-pulse">
+              <span
+                className={`${
+                  isSwitchOn ? "text-green-800" : "text-green-400"
+                }`}
+              >
+                Budget Buddy
+              </span>
+            </h2>
+            <h2
+              className={`text-center text-2xl m-2 font-bold ${
+                isSwitchOn ? "text-grey-100" : "text-gray-200"
+              } `}
+            >
+              Register to Us
+            </h2>
           </div>
 
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form
-            className="space-y-6"
-            action="#"
-            method="POST"
-            onSubmit={submitHandler}
-          >
-            <div>
-              <label
-                htmlFor="email"
-                className="pl-1 block text-sm font-medium leading-6 text-gray-900"
-              >
-                Username
-              </label>
-              <div className="mt-2">
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  autoComplete="name"
-                  required
-                  onChange={(e) => setName(e.target.value)}
-                  className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
 
-            <div>
-              <div className="flex items-center justify-between">
+          <div className={`rounded-lg p-2 ${
+            isSwitchOn ? "hover:bg-neutral-500" : "hover:bg-gray-900"
+          } sm:mx-auto sm:w-full sm:max-w-sm transition`}>
+            <form
+              className="space-y-6"
+              action="#"
+              method="POST"
+              onSubmit={submitHandler}
+            >
+              <div>
                 <label
-                  htmlFor="password"
-                  className="pl-1 block text-sm font-medium leading-6 text-gray-900"
+                  htmlFor="email"
+                  className={`ml-1 ${
+                    isSwitchOn ? "text-gray-200" : "text-white"
+                  } block text-sm font-medium leading-6 text-gray-900`}
                 >
-                  Password
+                  Username
                 </label>
+                <div className="mt-2">
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    autoComplete="name"
+                    required
+                    onChange={(e) => setName(e.target.value)}
+                    className={`pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset ${
+                      isSwitchOn
+                        ? "focus:ring-gray-300"
+                        : "focus:ring-indigo-600"
+                    } sm:text-sm sm:leading-6`}
+                  />
+                </div>
               </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="pl-1 block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Confirm Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="cpassword"
-                  name="cpassword"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  onChange={(e) => setConfirmpassword(e.target.value)}
-                  className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
 
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="pl-1 block text-sm font-medium leading-6 text-gray-900"
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className={`ml-1 ${
+                      isSwitchOn ? "text-gray-200" : "text-white"
+                    } block text-sm font-medium leading-6 text-gray-900`}
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    className={`pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset ${
+                      isSwitchOn
+                        ? "focus:ring-gray-300"
+                        : "focus:ring-indigo-600"
+                    } sm:text-sm sm:leading-6`}
+                  />
+                </div>
+              </div>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className={`ml-1 ${
+                      isSwitchOn ? "text-gray-200" : "text-white"
+                    } block text-sm font-medium leading-6 text-gray-900`}
+                  >
+                    Confirm Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="cpassword"
+                    name="cpassword"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    onChange={(e) => setConfirmpassword(e.target.value)}
+                    className={`pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset ${
+                      isSwitchOn
+                        ? "focus:ring-gray-300"
+                        : "focus:ring-indigo-600"
+                    } sm:text-sm sm:leading-6`}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className={`ml-1 ${
+                      isSwitchOn ? "text-gray-200" : "text-white"
+                    } block text-sm font-medium leading-6 text-gray-900`}
+                  >
+                    Upload your image
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="pic"
+                    name="pic"
+                    type="file"
+                    autoComplete="pic"
+                    required
+                    onChange={(e) => postDetails(e.target.files[0])}
+                    className={`pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset ${
+                      isSwitchOn
+                        ? "focus:ring-gray-300"
+                        : "focus:ring-indigo-600"
+                    } sm:text-sm sm:leading-6`}
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className={`px-6 py-3 bg-blue-500 text-white border border-white rounded-md transition-opacity hover:opacity-75 focus:outline-none focus:shadow-outline-blue flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm ${
+                    isSwitchOn
+                      ? "bg-neutral-500 hover:bg-neutral-400"
+                      : "bg-gray-900 hover:bg-gray-700"
+                  } focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+                  disabled={picLoading}
                 >
-                  Upload your image
-                </label>
+                  Register
+                </button>
               </div>
-              <div className="mt-2">
-                <input
-                  id="pic"
-                  name="pic"
-                  type="file"
-                  autoComplete="pic"
-                  required
-                  onChange={(e) => postDetails(e.target.files[0])}
-                  className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-red-500 hover:bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                disabled={picLoading}
-              >
-                Register
-              </button>
-            </div>
-          </form>
-        </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
