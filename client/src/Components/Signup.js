6fc9fa60bc3629baebcdc58313990ba4680eb82b
Madhurgupta12 <om96002@gmail.com>
2023-11-25 03:07:41 +0530

    HomePageDone

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -12,7 +12,9 @@ import { ToastContainer } from "react-toastify";
 import WelcomeToaster from "./WelcomeToaster";
 import logo from "./immg.jpeg";
 import "./HomePageStyle.css";
-
+import StyledFrontPage from "./FrontPage";
+import "./ff.css";
+import ResponsiveBackground from "./FrontPage";
 import Timeline from "./TimeLine";
 import { useToast } from "@chakra-ui/react";
 const Signup = () => {
@@ -88,28 +90,41 @@ const Signup = () => {
       <WelcomeToaster></WelcomeToaster>
       <ToastContainer />
       {/* <HHH></HHH> */}
-      
-        <div className=" dddt flex flex-col h-screen justify-end items-start">
-          <div className=" flex justify-end items-end h-screen ">
-            <button className="m-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-              <Link to="/emailcheck">Login</Link>
-            </button>
-            <button className="m-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-              <Link to="/authemail">Signup</Link>
-            </button>
+      <div className="  ttt flex items-center flex-col h-screen justify-end items-start">
+        <div className="mt-5  p-5 tttt rounded-lg  bg-gradient-to-r from-blue-500 to-purple-500 min-h-screen flex flex-col items-center justify-center text-white">
+          <div className="text-center">
+            <h1 className="text-5xl font-extrabold mb-4 tracking-wide">
+              Tech Buddy
+            </h1>
+            <p className="text-lg font-light leading-loose">
+              Your Smart Expense Tracker
+            </p>
           </div>
-
-          <div className="">
-            <GoogleLogin
-              clientId="504611249331-3l8vq2l7k662aof3rllqu4ldeo9cr535.apps.googleusercontent.com"
-              buttonText="Continue with Google"
-              onSuccess={responseGoogle}
-              onFailure={failure}
-              cookiePolicy={"single_host_origin"}
-            />
+          <div className="mt-8">
+            <button className="bg-white text-blue-500 px-4 py-2 rounded-full shadow-md hover:bg-green-600 hover:text-white transition duration-300">
+              <div className="ml-0 flex justify-end items-end ">
+                <button className="m-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-green-600">
+                  <Link to="/emailcheck">Login</Link>
+                </button>
+                <button className="m-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-green-600">
+                  <Link to="/authemail">Signup</Link>
+                </button>
+              </div>
+            </button>
+            <div className=" ml-3 mt-2">
+          <GoogleLogin
+            clientId="504611249331-3l8vq2l7k662aof3rllqu4ldeo9cr535.apps.googleusercontent.com"
+            buttonText="Continue with Google"
+            onSuccess={responseGoogle}
+            onFailure={failure}
+            cookiePolicy={"single_host_origin"}
+          />
+        </div>
           </div>
         </div>
-    
+
+        
+      </div>
       ;<Body></Body>
       <Path></Path>
       <Footer></Footer>
